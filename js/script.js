1ed2f48e3e4a4f2bb74f844150ada7348c78484c
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-20 23:45:24 -0700

    Refactor: separate business and user interface logic and count checked checkbox using .length instead of loop.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,25 @@
+//Business logic
+const suggestion = (message) => {
+  $("#message").text(message);
+}
+
+//Frontend logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var signOfStress = 0;
-    var healthSymptoms = 0;
-    var copingMethod = 0;
-    var questions = ['signOfStress', 'healthSymptoms', 'copingMethod'];
-    for(var i = 0; i < questions.length; i++) {
-      $(`input:checkbox[name=${questions[i]}]:checked`).each(function(){
-        // questions[i] +=;
-        console.log(questions[i];
-        if (questions[0] > 4 && questions[1] <= 2 && questions[2] > 4) {
-          $("#recommendation").append("<p>It's not stress related. You should see your physician.</p>");
-        }
-      })
-    }
-    // This is working with empty array!
-    // $("input:checkbox[name=signOfStress]:checked").each(function(){
-    //   var checkedSignOfStress = $(this).val();
-    //   signOfStress.push(checkedSignOfStress);
-    // });
-    // $("input:checkbox[name=healthSymptoms]:checked").each(function(){
-    //   var checkedHealthSymptoms = $(this).val();
-    //   healthSymptoms.push(checkedHealthSymptoms);
-    // });
-    // $("input:checkbox[name=copingMethod]:checked").each(function(){
-    //   var checkedCopingMethod = $(this).val();
-    //   copingMethod.push(checkedCopingMethod);
-    // });
+    var checkedSignOfStress= $("input:checkbox[name=sign-of-stress]:checked").length;
+    var checkedHealthSymptoms = $("input:checkbox[name=health-symptoms]:checked").length;
+    var checkedCopingMethod = $("input:checkbox[name=coping-method]:checked").length;
 
-    // if (signOfStress > 4 && healthSymptoms <= 2 && copingMethod > 4) {
-    //   $("#recommendation").append("<p>It's not stress related. You should see your physician.</p>");
-     if (signOfStress > 3 && copingMethod > 4) {
-      $("#recommendation").append("<p>Great job. You're trying to dealing with your stress.</p>");
-    } else if (signOfStress > 3 && healthSymptoms > 4 ) {
-      $("#recommendation").append("<p>Let's work out more.</p>");
+    if (checkedSignOfStress > 4 && checkedHealthSymptoms <= 2 && checkedCopingMethod > 4) {
+      suggestion("It's not stress related. You should see your physician.");
+    } else if (checkedSignOfStress > 3 && checkedCopingMethod > 4) {
+      suggestion("Great job. You're trying to deal with your stress.");
+    } else if (checkedSignOfStress > 3 && checkedHealthSymptoms > 4 ) {
+      suggestion("Your situation is a little bit serious.");
     }  else {
-      $("#recommendation").append("<p>Let's learn how to deal with stress.</p>");
+      suggestion("Let's learn how to deal with stress with us.");
     }
-
     $("#recommendation").show();
   });
 });
